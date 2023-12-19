fc7dd0463c2cdd510b2d052ae0a2c1e42147dca6
fabiangunzinger <fa.gunzinger@gmail.com>
2023-12-19 10:00:53 +0000

    Add CUPED

--- fabutils/stats/exp_eval.py
+++ fabutils/stats/exp_eval.py
@@ -19,3 +19,23 @@ def wls(df, metric):
     results = model.fit()
     return results.pvalues["is_treated"]
 
+def traditional_cuped(df, metric):
+    """Run traditional CUPED and return p-value."""
+    
+    def _cuped_adjusted_metric(df, metric, metric_pre):
+        y = df[metric].values
+        x = df[metric_pre].values
+        valid_indices = (~np.isnan(y)) & (~np.isnan(x))
+        y_valid, x_valid = y[valid_indices], x[valid_indices]
+        m = np.cov(y_valid, x_valid)
+        theta = m[0, 1] / m[1, 1]
+        return (y - (x - np.nanmean(x)) * theta)
+
+    # Perform experiment evaluation and return p-value
+    # (Use WLS to be consistent with CausalJet)
+    y = _cuped_adjusted_metric(df, metric, f"{metric}_pre")
+    x = sm.add_constant(df["is_treated"].astype(float))
+    w = df["assignments_freq"]
+    model = sm.WLS(endog=y, exog=x, weights=w)
+    results = model.fit()
+    return results.pvalues["is_treated"]
